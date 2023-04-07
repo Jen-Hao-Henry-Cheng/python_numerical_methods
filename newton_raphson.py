9de0dbc072de6af6e9f8a0a5a5d5346fc0414e69
HenryCheng <henry25097733@gmail.com>
2023-04-07 23:40:37 +0800

    feat: add main file of gradient descent

--- newton_raphson.py
+++ newton_raphson.py
@@ -4,7 +4,6 @@ def newton_raphson(f, df, x0, tol=1.0e-09, maxIter=100):
         xkp1 = xk - 1*f(xk)/df(xk)
         error = (xkp1 - xk)
         if abs(error) < tol:
-            return xkp1, k
+            break
         xk = xkp1
-        print(f'k = {k+1}, {xk=}, {error=}')
-    print('Too many iteration\n')
+    return xkp1, k
